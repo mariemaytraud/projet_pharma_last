50a25af249123cda8bec902141158796592d95a9
mariemaytraud <maytraudmarie@gmail.com>
2026-03-07 14:16:48 +0100

    changement pour suppression

--- src/main/java/pharmacie/entity/Medicament.java
+++ src/main/java/pharmacie/entity/Medicament.java
@@ -25,68 +25,72 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@ToString
 public class Medicament {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Setter(AccessLevel.NONE) // la clé est autogénérée par la BD, On ne veut pas de "setter"
-	private Integer reference = null;
 
-	@NonNull // Lombok, génère une vérification dans le constructeur par défaut
-	@Column(unique=true, length = 255)
-	private String nom;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE) // la clé est autogénérée par la BD, On ne veut pas de "setter"
+    private Integer reference = null;
 
-	private String quantiteParUnite = "Une boîte de 12";
+    @NonNull // Lombok, génère une vérification dans le constructeur par défaut
+    @Column(unique = true, length = 255)
+    private String nom;
 
-	@PositiveOrZero
-	private BigDecimal prixUnitaire = BigDecimal.TEN;
+    private String quantiteParUnite = "Une boîte de 12";
 
-	/**
-	 * Nombre d'unités en stock
-	 * Décrémenté quand on expédie une commande contenant ce médicament
-	 */
-	@ToString.Exclude
-	@PositiveOrZero
-	private int unitesEnStock = 0;
+    @PositiveOrZero
+    private BigDecimal prixUnitaire = BigDecimal.TEN;
 
-	/**
-	 * Nombre d'unités "en commande"
-	 * Un médicament est "en commande" si il est dans une commande qui n'est pas encore expédiée
-	 * Incrementé quand on ajoute des unités de ce médicament à une ligne de commande
-	 * Décrémenté quand on expédie une commande contenant ce médicament
-	 */
-	@ToString.Exclude
-	@PositiveOrZero
-	private int unitesCommandees = 0;
+    /**
+     * Nombre d'unités en stock Décrémenté quand on expédie une commande
+     * contenant ce médicament
+     */
+    @ToString.Exclude
+    @PositiveOrZero
+    private int unitesEnStock = 0;
 
-	/**
-	 * Niveau de reapprovisionnement
-	 * Si le stock devient inférieur ou égal à ce niveau, 
-	 * on doit approvisionner de nouvelles unités de ce médicament auprès d'un fournisseur
-	 */
-	@ToString.Exclude
-	@PositiveOrZero
-	private int niveauDeReappro = 0;
+    /**
+     * Nombre d'unités "en commande" Un médicament est "en commande" si il est
+     * dans une commande qui n'est pas encore expédiée Incrementé quand on
+     * ajoute des unités de ce médicament à une ligne de commande Décrémenté
+     * quand on expédie une commande contenant ce médicament
+     */
+    @ToString.Exclude
+    @PositiveOrZero
+    private int unitesCommandees = 0;
 
-	/**
-	 * Indique si le médicament est indisponible
-	 */
-	@ToString.Exclude
-	private boolean indisponible = false;
+    /**
+     * Niveau de reapprovisionnement Si le stock devient inférieur ou égal à ce
+     * niveau, on doit approvisionner de nouvelles unités de ce médicament
+     * auprès d'un fournisseur
+     */
+    @ToString.Exclude
+    @PositiveOrZero
+    private int niveauDeReappro = 0;
 
-	@Column(length = 500)
-	private String imageURL;
+    /**
+     * Indique si le médicament est indisponible
+     */
+    @ToString.Exclude
+    private boolean indisponible = false;
 
-	@ToString.Exclude
-	@JsonIgnoreProperties("medicaments") // pour éviter la boucle infinie si on convertit le médicament en JSON
-	@NonNull // Lombok, génère une vérification dans le constructeur par défaut
-	@ManyToOne(optional = false) // La clé étrangère ne peut pas être nulle dans la table Medicament
-	private Categorie categorie ;
+    @Column(length = 500)
+    private String imageURL;
 
-	@ToString.Exclude
-	@JsonIgnore // On n'inclut pas les lignes quand on convertit le médicament en JSON
-	@OneToMany(mappedBy = "medicament", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
-	private List<Ligne> lignes = new LinkedList<>();
+    @ToString.Exclude
+    @JsonIgnoreProperties("medicaments") // pour éviter la boucle infinie si on convertit le médicament en JSON
+    @NonNull // Lombok, génère une vérification dans le constructeur par défaut
+    @ManyToOne(optional = false) // La clé étrangère ne peut pas être nulle dans la table Medicament
+    private Categorie categorie;
 
+    @ToString.Exclude
+    @JsonIgnore // On n'inclut pas les lignes quand on convertit le médicament en JSON
+    @OneToMany(mappedBy = "medicament", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.REMOVE})
+    private List<Ligne> lignes = new LinkedList<>();
 
 }
